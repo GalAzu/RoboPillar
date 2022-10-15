1af25b067d865dc1767f53e949186fbb04ebcbdc
GalAzu <60822668+GalAzu@users.noreply.github.com>
2022-10-15 13:48:09 +0100

    buildMechanics

---
+++ GameJamPlusTeam02/Assets/BuildMechanics.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildMechanics : MonoBehaviour
+{
+    
+    public float distance;
+    public float buildingCooldownTime;
+    public float buildingCooldownTimer;
+    private Inventory inventory;
+    public Dictionary<int, WasteInventory> wasteQuantityToBuild = new Dictionary<int, WasteInventory>();
+
+    private void Start()
+    {
+        buildingCooldownTimer = buildingCooldownTime;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        buildingCooldownTimer -= Time.deltaTime;
+        if(Input.GetKeyDown(KeyCode.Mouse0) && buildingCooldownTimer <= 0)
+        {
+            buildingCooldownTimer = buildingCooldownTime;
+            CreateObjectAtPlace();
+        }
+        print("buildingCooldownTimer " + buildingCooldownTimer.ToString());
+    }
+    private void CreateObjectAtPlace()
+    {
+        Vector3 transformToBuild = transform.TransformPoint(Vector3.forward * distance);
+        
+        Instantiate(objToBuild, transformToBuild, transform.rotation);
+    }
+}
