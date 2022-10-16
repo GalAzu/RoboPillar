d260a9dfc4dc055ad4e82ee89330f9c768328941
CrazyPel <86598363+CrazyPel@users.noreply.github.com>
2022-10-16 12:34:16 +0300

    ui sounds

--- GameJamPlusTeam02/Assets/Scripts/Audio/UISounds.cs
+++ GameJamPlusTeam02/Assets/Scripts/Audio/UISounds.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class UISounds : MonoBehaviour
+    
 {
+   
     // Start is called before the first frame update
     void Start()
     {
