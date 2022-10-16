1240d406745a6464809809bf18be06dca35d1429
GalAzu <60822668+GalAzu@users.noreply.github.com>
2022-10-16 13:33:00 +0100

    lighton and off

--- GameJamPlusTeam02/Assets/Scripts/Towers.cs
+++ GameJamPlusTeam02/Assets/Scripts/Towers.cs
@@ -7,9 +7,14 @@ public class Towers : MonoBehaviour
     [SerializeField] private float towerDetectionRadius;
     [SerializeField] private List<Collider> wasteInRadius = new List<Collider>();
     [SerializeField] private LayerMask wasteMask;
+    [SerializeField] Light light;
+    [SerializeField]
+    private float lightOff, lightOn;
+    [SerializeField]private float lightFadeRate;
 
     private void Start()
     {
+        light = GetComponent<Light>();
         Collider[] getWasteInRadiusCastColliders = Physics.OverlapSphere(transform.position, towerDetectionRadius, wasteMask);
         foreach(var collider in getWasteInRadiusCastColliders)
         {
@@ -19,9 +24,20 @@ public class Towers : MonoBehaviour
             }
         }
     }
+    private void Update()
+    {
+        TurnLightOnAndOff();
+    }
     private void OnDrawGizmos()
     {
         Gizmos.DrawWireSphere(transform.position, towerDetectionRadius);
         
     }
+
+    private void TurnLightOnAndOff()
+    {
+        if (wasteInRadius.Count > 3) light.intensity = Mathf.Lerp(light.intensity, lightOn, lightFadeRate);
+        else if (wasteInRadius.Count < 3) light.intensity = Mathf.Lerp(light.intensity, lightOff, lightFadeRate);
+    }
+
 }
